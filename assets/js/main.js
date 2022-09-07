e428016358e4c55e0b0258e8fcebb204a3406a23
Tracerract <tracerract@gmail.com>
2022-09-07 13:56:03 -0600

    JS fix
    JS from testing implementation did not initiate properly on live version, refactored to self initiate instead of assuming automatic.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,42 +5,46 @@
 function classAdd(a, b) { a.classList.add(b) }
 function classRemove(a, b) { a.classList.remove(b) }
 function toggle(a, b = true) {
-  if (b == true) {
-      classAdd(a, 'fade')
-    setTimeout( () => {classAdd(a, 'inactive')}, 500)
-  }
-  if (b == false) {
-    setTimeout( () => {classRemove(a, 'inactive')}, 500)
-    setTimeout( () => {classRemove(a, 'fade')}, 1000)
-  }
+    if (b == true) {
+        classAdd(a, 'fade')
+        setTimeout(() => { classAdd(a, 'inactive') }, 500)
+    }
+    if (b == false) {
+        setTimeout(() => { classRemove(a, 'inactive') }, 500)
+        setTimeout(() => { classRemove(a, 'fade') }, 1000)
+    }
 }
 // Get lists of elements
 const navLinks = document.getElementsByClassName('navlink')
 const panels = document.getElementsByClassName('panel')
 // Setup events for navlinks
-for (let i = 0; i < navLinks.length; i++) {
-    const elem = navLinks[i]
-    const href = elem.getAttribute('href')
-    elem.addEventListener('click', function (event) {
-        event.preventDefault()
-        event.stopPropagation()
-        if (window.location.hash != href) {
-            window.location.hash = href
-            initialize(i)
-        }
-    })
+function navigation() {
+    for (let i = 0; i < navLinks.length; i++) {
+        const elem = navLinks[i]
+        const href = elem.getAttribute('href')
+        elem.addEventListener('click', function (event) {
+            event.preventDefault()
+            event.stopPropagation()
+            if (window.location.hash != href) {
+                window.location.hash = href
+                initialize(i)
+            }
+        })
+    }
 }
+navigation()
+
 // initialize the loaded frame
 function initialize(val) {
-//   Skip transitions on page load
+    //   Skip transitions on page load
     if (val == undefined) {
-          for (let item of panels) {
+        for (let item of panels) {
             if (item != panels[0]) {
                 classAdd(item, 'inactive')
                 classAdd(item, 'fade')
             }
         }
-      return;
+        return;
     }
     let panel = panels[val]
     for (let item of panels) {
